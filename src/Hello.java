b49e7152439c70bd66fa07e9ec9bdd12aff0787c
Joe <Pretzelman718@gmail.com>
2019-05-19 00:19:22 -0600

    No but I can show you.

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Brian Bates.");
         System.out.println("Hello, Brian Bates, my name is Juheon Song");
+        System.out.println("Hello Harry Potter, my name is Tom Riddle");
     }
 }
