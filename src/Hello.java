0ab64d64ec5005dd91758671f46ee8f0200ed4a2
juheon97 <son16007@byui.edu>
2019-05-19 00:07:41 -0600

    fix

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Brian Bates.");
+        System.out.println("Hello, Brian Bates, my name is Juheon Song");
     }
 }
